e89d2c5d26cca5dfddc81747799a0cddb13e3ce2
loveflora <love7620640@naver.com>
2023-07-26 14:53:36 +0900

    <study> js 자료형, 형변환

--- 04-JS/index3.js
+++ 04-JS/index3.js
@@ -1,4 +1,4 @@
-console.log("connected!");
+console.log('connected!');
 
 // 변수 선언: 변수 정의
 // 변수 할당: = 연산자를 이용해 값을 넣음
@@ -70,15 +70,15 @@ console.log(c);
 // 1. string (문자열)
 // : 텍스트 정보, 따옴표 감싸야 함
 // : 숫자, 특수문자도 따옴표 안에 포함? 문자열
-let text = "안녕하세요!";
-let text2 = "하이!";
-let gender = "남자";
+let text = '안녕하세요!';
+let text2 = '하이!';
+let gender = '남자';
 console.log(text);
 console.log(text2);
 console.log(text + text2);
-console.log(text, text2, "ㅋㅋ");
+console.log(text, text2, 'ㅋㅋ');
 
-console.log(text + "저는 Sean 입니다. 저의 성별은" + gender + "입니다!");
+console.log(text + '저는 Sean 입니다. 저의 성별은' + gender + '입니다!');
 // 템플릿 리터럴 (es6) 백틱(`)와 달러, 중괄호(${}) 조합으로 사용
 // : 문자열 내에서 변수와, 상수를 간결히 표현
 // 훨씬 간결해짐
@@ -106,7 +106,7 @@ console.log(isWater, hasApple);
 // "값이 없음"을 의도적으로 명시
 let temp = null;
 console.log(temp);
-temp = "Hello~";
+temp = 'Hello~';
 console.log(temp);
 
 // 5. undefined
@@ -117,9 +117,8 @@ console.log(x); // undefined
 // [QUIZ] 변수
 // Q1. 코드 실행시 q1 변수는 어떤 값이 될까?
 let q1 = 3;
-
-console.log(q1 - 2);
-// 1
+q1 - 2;
+// 3
 
 // Q2. 다음 코드 실행시 q2 변수는 어떤 값이 될까?
 let q2 = 10;
@@ -129,5 +128,112 @@ console.log(q2);
 
 // Q3. 다음 코드 실행시 q3 변수는 어떤 값이 될까?
 const q3 = 1;
-q3 = q3 + 1; // error
+// q3 = q3 + 1; // error
 console.log(q3 + 1);
+
+// 6. 배열
+// 배열 크기(길이) : 원소 개수와 동일
+const fruits = ['orange', 'pineapple', 'grape', 'apple'];
+console.log(fruits);
+console.log(fruits[0]); // fruits 배열의 위치가 0인 원소 출력
+
+// js에서는 배열 원소의 자료형이 달라도 됨!
+const data = [1, 'allie', false, null, undefined];
+console.log(data);
+
+// Array 안에 Array 들어갈 수 있음 (중첩 가능) => 2차원 배열
+const korean = [
+  ['가', '나', '다'],
+  ['라', '마', '바'],
+  ['사', '아', '자'],
+];
+
+console.log(korean);
+console.log(korean[0]);
+console.log(korean[1]);
+console.log(korean[2]);
+
+console.log(korean[0][2]);
+
+// 가자 출력
+console.log(korean[0][0] + korean[2][2]);
+
+// 퀴즈 3차원 배열에서 숫자 8 출력
+const nums = [
+  [
+    [1, 2, 3],
+    [4, 5, 6],
+  ],
+  [
+    [7, 8, 9],
+    [10, 11, 12],
+  ],
+];
+
+console.log(nums[1][0][1]);
+
+// 7. object (key: value)
+const cat = {
+  name: '나비',
+  age: 1,
+  isCute: true,
+  mew: function () {
+    return '냐옹';
+  },
+};
+console.log(cat); // object
+
+// object의 속성(key)에 접근하는 방법
+// (1) 점 표기법
+console.log(cat.name);
+console.log(cat.age);
+console.log(cat.mew());
+// (2) 대괄호 표기법
+console.log(cat['name']);
+// key가 변수에 저장되어 있을 때 주로 사용
+const tempVal = 'name';
+console.log(cat[tempVal]); // && cat.name과 동일
+
+const me = {
+  name: '세화',
+  isLeader: false,
+  job: 'developer',
+  interest: ['music', 'movie', 'exercise'],
+};
+
+console.log(me);
+
+console.log(typeof null);
+// JS 언어 자체의 오류
+
+// 형 변환
+// let mathScore = prompt('수학점수 입력');
+// let engScore = prompt('영어점수 입력');
+// let avg = (mathScore + engScore) / 2;
+// console.log(typeof mathScore);
+// console.log(typeof engScore);
+// console.log(`수학 영어 평균점수는 ${avg}점 입니다`);
+// prompt로 입력받은 값은 '문자'로 저장됨
+
+// 1. String()
+let str1 = true;
+console.log(typeof String(null));
+console.log(str1.toString(), typeof str1.toString());
+
+// 2. Number()
+let n1 = true;
+let n3 = '123.9';
+
+console.log(Number(n1), typeof Number(n1));
+console.log(parseInt(n3, 10)); // n3 값을 10진수의 정수(int)로 바꾸겠다 --> 123
+console.log(parseFloat(n3)); // n3 값을 실수로 바꾸겠다 --> 123.9
+
+let mathScore = '77';
+let engScore = '88';
+let avgScore = (Number(mathScore) + Number(engScore)) / 2;
+
+console.log(avgScore);
+
+// +) null, undefined를 숫자로 바꾼다면?
+console.log(Number(undefined), typeof Number(undefined)); // NaN, number
+console.log(Number(null), typeof Number(null)); // 0, number
