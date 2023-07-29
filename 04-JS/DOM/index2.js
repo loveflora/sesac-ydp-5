2d0176668192cf057dda7dc14e83ff151acc0e3b
sehwa <love7620640@naver.com>
2023-07-30 00:04:28 +0900

    <assignment> end !

--- 04-JS/DOM/index2.js
+++ 04-JS/DOM/index2.js
@@ -1,9 +1,9 @@
-const div1 = document.getElementById('div1');
+const div1 = document.getElementById("div1");
 console.log(div1);
 
 //=== 1. 태그 내부 내용 변경 ===
 //; 1) innerHTML : 태그 사용O
-div1.innerHTML = '여기는 <b>첫번째</b> 태그';
+div1.innerHTML = "여기는 <b>첫번째</b> 태그";
 console.log(div1);
 //-- 2) innerText : 태그가 문자(기호) 그대로 노출 (b 태그 그대로 노출)
 // div1.innerText = '여기는 <b>첫번째</b> 태그';
@@ -14,25 +14,25 @@ console.log(div1);
 
 //=== 2. 속성(attribute) ===
 //-- 1) 속성 값 '설정' : setAttribute(속성명, 변경할 속성값)
-const naver = document.getElementById('naver');
-naver.setAttribute('href', 'https://www.google.com');
+const naver = document.getElementById("naver");
+naver.setAttribute("href", "https://www.google.com");
 
-const pooh = document.getElementById('pooh');
-pooh.setAttribute('src', './img/cat.jpeg');
+const pooh = document.getElementById("pooh");
+pooh.setAttribute("src", "./img/cat.jpeg");
 
 //-- 2) 속성 값 '얻기' : getAttribute(속성명)
-console.log(document.getElementById('pooh').getAttribute('src'));
+console.log(document.getElementById("pooh").getAttribute("src"));
 
-// 참고) 속성 접근 (.) 연산자로도 가능
-console.log(document.getElementById('pooh').id);
-console.log(document.getElementById('naver').href);
+//-- 3) 속성 접근 : . 연산자로도 가능
+console.log(document.getElementById("pooh").id);
+console.log(document.getElementById("naver").href);
 
-// 참고) . 연산자로 속성에 접근하고 = 할당 연산자로 속성 값 변경 가능
-document.getElementById('naver').href = '#';
+//-- 4) 변경 : . 연산자로 속성에 접근하고 = 할당 연산자로 속성 값 변경 가능
+document.getElementById("naver").href = "#";
 
 //=== 3. CSS 지정 ===
-const h1 = document.querySelector('h1');
-const list = document.querySelectorAll('ul > li'); // 유사 배열
+const h1 = document.querySelector("h1");
+const list = document.querySelectorAll("ul > li"); // 유사 배열
 
 //; 1) style 속성
 // style 속성 이용해 CSS 지정 가능
@@ -41,9 +41,9 @@ const list = document.querySelectorAll('ul > li'); // 유사 배열
 // list[0].style.fontSize = '20px';
 // list[0].style.color = 'yellow';
 for (let li of list) {
-  li.style.backgroundColor = 'purple';
-  li.style.fontSize = '20px';
-  li.style.color = 'yellow';
+  li.style.backgroundColor = "purple";
+  li.style.fontSize = "20px";
+  li.style.color = "yellow";
 }
 
 //; 2) classList 활용
@@ -51,20 +51,20 @@ for (let li of list) {
 // xxx.classList.remove
 // xxx.classList.contains : 유무 확인 (T/F)
 // xxx.classList.toggle : 있으면 제거, 없으면 추가
-h1.classList.add('add-h1');
+h1.classList.add("add-h1");
 // h1.classList.remove('add-h1');
-console.log(h1.classList.contains('add-h1'));
+console.log(h1.classList.contains("add-h1"));
 
-h1.classList.contains('add-h1')
-  ? (h1.innerText = 'add-h1 클래스 있음')
-  : (h1.innerText = 'add-h1 클래스 없음');
+h1.classList.contains("add-h1")
+  ? (h1.innerText = "add-h1 클래스 있음")
+  : (h1.innerText = "add-h1 클래스 없음");
 
-h1.classList.toggle('add-h1');
+h1.classList.toggle("add-h1");
 
 //=== 4. 요소 찾기 ===
 // 계층 구조 (형제, 자식, 부모, 조상, 자손)
-const friends = document.querySelector('#friends');
-const tigger = document.querySelector('#tigger');
+const friends = document.querySelector("#friends");
+const tigger = document.querySelector("#tigger");
 
 //; 1) 자식 요소
 console.log(friends.children); // 유사배열 --> 자식 모두 선택
@@ -80,37 +80,37 @@ console.log(tigger.nextElementSibling);
 console.log(tigger.nextElementSibling.nextElementSibling);
 
 //=== 5. 요소 생성, 추가, 삭제 ===
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
 
 //; 1) 생성
-const p = document.createElement('p'); // 요소 생성
+const p = document.createElement("p"); // 요소 생성
 // js로 <p></p> 태그 생성
 console.log(p);
-p.innerText = '새로 추가된 p 요소입니다.';
+p.innerText = "새로 추가된 p 요소입니다.";
 p.style.fontWeight = 700;
-p.style.backgroundColor = 'red';
+p.style.backgroundColor = "red";
 
 //; 2) 추가
 //-- (1) x.append(y)
 //-- ((1)) x.add(y) : x 요소의 맨 마지막 자식으로 y 요소 추가
 container.append(p);
 
-const p2 = document.createElement('p'); // 요소 생성
-const p3 = document.createElement('p'); // 요소 생성
-p2.innerHTML = 'p2';
-p3.innerHTML = 'p3';
-p2.classList.add('p-2');
-p3.classList.add('p-3');
+const p2 = document.createElement("p"); // 요소 생성
+const p3 = document.createElement("p"); // 요소 생성
+p2.innerHTML = "p2";
+p3.innerHTML = "p3";
+p2.classList.add("p-2");
+p3.classList.add("p-3");
 
 container.append(p2, p3); // 여러개 추가도 가능 !
 //: appendChild 는 한 번에 하나만 추가가능
 
 //-- ((2)) x.prepend(y) : x요소의 맨 처음 자식으로 y요소 추가
-const li1 = document.createElement('li');
-li1.textContent = '처음';
+const li1 = document.createElement("li");
+li1.textContent = "처음";
 friends.prepend(li1);
-const li0 = document.createElement('li');
-li0.innerHTML = '<b>친구들을 소개합니다</b>';
+const li0 = document.createElement("li");
+li0.innerHTML = "<b>친구들을 소개합니다</b>";
 friends.prepend(li0);
 
 //-- (2) x.appendChild(y)
@@ -119,7 +119,7 @@ div1.appendChild(p);
 
 //; 3) 삭제
 //-- (1) x.remove(y) : x요소 삭제
-const firstLi = document.querySelector('li');
+const firstLi = document.querySelector("li");
 console.log(firstLi);
 // firstLi.remove();
 
--- 04-JS/DOM/practice02.js
+++ 04-JS/DOM/practice02.js
@@ -1,10 +1,60 @@
-const name = document.getElementById('name');
-const content = document.getElementById('content');
-const add = document.getElementById('add');
+const spanList = document.querySelectorAll("span");
+const table = document.querySelector("table");
+const thList = table.querySelectorAll("th");
+const name = document.getElementById("name");
+const content = document.getElementById("content");
+const add = document.getElementById("add");
 
-function addHandler() {
-  const td = document.createElement('td');
-  td.innerHTML = name.value;
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth() + 1;
+const day = date.getDate();
+const hour = date.getHours();
+const minute = date.getMinutes();
+
+for (let span of spanList) {
+  span.style.textAlign = "center";
+  span.style.display = "inline-block";
+  span.style.width = "50px";
 }
 
-add.addEventListener('click', () => addHandler());
+for (let th of thList) {
+  th.style.border = "1px solid black";
+}
+
+content.style.width = "500px";
+
+table.style.border = "1px solid";
+table.style.textAlign = "center";
+
+table.style.width = "500px";
+
+let idx = 0;
+
+function addHandler() {
+  const tr = document.createElement("tr");
+  const td1 = document.createElement("td");
+  const td2 = document.createElement("td");
+  const td3 = document.createElement("td");
+  const td4 = document.createElement("td");
+
+  idx += 1;
+
+  td1.innerHTML = idx;
+  td2.innerHTML = name.value;
+  td3.innerHTML = content.value;
+  td4.innerHTML = `${year}-${month}-${day} ${hour} : ${minute}`;
+
+  document.querySelector("tbody").append(tr, td1, td2, td3, td4);
+
+  const tdList = table.querySelectorAll("td");
+
+  for (let td of tdList) {
+    td.style.border = "1px solid black";
+  }
+
+  name.value = "";
+  content.value = "";
+}
+
+add.addEventListener("click", () => addHandler());
