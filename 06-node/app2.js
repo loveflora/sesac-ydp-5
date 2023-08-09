426f143b59e627bfa95c5384480df9d1106b7f5c
loveflora <love7620640@naver.com>
2023-08-09 15:08:26 +0900

    <study> module

---
+++ 06-node/app2.js
@@ -0,0 +1,12 @@
+const math = require('./math2');
+// const math = {
+//   add,
+//   PI,
+//   E,
+// };
+
+console.log(math.add(math.PI, math.E));
+
+console.log(math.subtract(math.PI, math.E));
+console.log(math.multiply(3, 2));
+console.log(math.divide(3, 2));
---
+++ 06-node/math.js
@@ -0,0 +1,5 @@
+//] math module : add 함수를 정의한 파일
+const add = (a, b) => a + b;
+
+// add 함수를 다른 js 파일에서 쓸 수 있도록 내보내기
+module.exports = add;
