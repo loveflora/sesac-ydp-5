52783036c0a3d6a3d3cee7ec72947050006849a5
loveflora <love7620640@naver.com>
2023-08-14 15:38:19 +0900

    <study> promise_chaining

--- 08-async/2_promise.js
+++ 08-async/2_promise.js
@@ -8,22 +8,22 @@
 // rejected : 실패
 
 //] 1. promise 생성하는 코드
-function promise1(flag) {
-  // new Promise 생성
-  return new Promise(function (resolve, reject) {
-    if (flag) {
-      resolve(
-        `현재 프로미스 상태는 fulfilled(이행)! then 메서드로 연결. 
-이때 flag 값은 ${flag}`
-      );
-    } else {
-      reject(
-        `현재 프로미스 상태는 rejected(거절)! catch 메서드로 연결. 
-이때 flag 값은 ${flag}`
-      );
-    }
-  });
-}
+// function promise1(flag) {
+//   // new Promise 생성
+//   return new Promise(function (resolve, reject) {
+//     if (flag) {
+//       resolve(
+//         `현재 프로미스 상태는 fulfilled(이행)! then 메서드로 연결.
+// 이때 flag 값은 ${flag}`
+//       );
+//     } else {
+//       reject(
+//         `현재 프로미스 상태는 rejected(거절)! catch 메서드로 연결.
+// 이때 flag 값은 ${flag}`
+//       );
+//     }
+//   });
+// }
 
 //] 2. promise 사용(소비)하는 코드
 // promise1(5 < 3)
@@ -35,6 +35,53 @@ function promise1(flag) {
 //     console.log('error');
 //   });
 
-promise1(5 < 3)
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err));
+// 화살표 함수로도 가능 !
+// promise1(5 < 3)
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
+
+//] 예시
+// "콜백함수" 이용해 동기처리한 것을 "promise" 이용해 동기처리
+function goMart() {
+  console.log('마트에 가서 어떤 음료를 살지 고민한다.');
+}
+
+function pickDrink() {
+  // Promise 사용하는 객체
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      console.log('고민 끝 !');
+      product = '제로 콜라';
+      price = 4000; // 2000 4000
+      // resolve(); // 성공에 대한 값이 없이, 그냥 "성공"을 의미
+
+      if (price <= 2000) {
+        resolve();
+      } else {
+        reject();
+      }
+    }, 3000);
+  });
+}
+
+function pay() {
+  console.log(`상품명: ${product}, 가격: ${price}`);
+}
+
+function nopay() {
+  console.log('금액 부족');
+}
+
+let product;
+let price;
+goMart();
+pickDrink().then(pay).catch(nopay);
+
+//\ 아래 1), 2) 동일한 함수
+//1)
+//   pickDrink().then(pay);
+
+// 2)
+//   pickDrink().then(function() {
+//      pay();
+//   })
---
+++ 08-async/3_promise-chaining.js
@@ -0,0 +1,88 @@
+//=== promise chaining ===
+// 함수를 이용해 (4+3)*2-1 = 13을 연산해보자 !
+// sub(mul(add(4, 3), 2), 1) : add -> mul -> sub
+
+//] case 1. 콜백함수로 처리한다면 ?
+// function add(n1, n2, callback) {
+//   setTimeout(function () {
+//     const result = n1 + n2; // 7
+//     callback(result); // callback(7)
+//   }, 1000);
+// }
+
+// function mul(n, callback) {
+//   setTimeout(function () {
+//     const result = n * 2;
+//     callback(result);
+//   }, 700);
+// }
+
+// function sub(n, callback) {
+//   setTimeout(function () {
+//     const result = n - 1;
+//     callback(result); // callback(13)
+//   }, 500);
+// }
+
+//-- 콜백지옥
+// add(4, 3, function (x) {
+//   console.log('1: ', x); // 7
+//   mul(x, function (y) {
+//     console.log('2: ', y); // 14
+//     sub(y, function (z) {
+//       console.log('3: ', z);
+//     });
+//   });
+// });
+
+//] case 2. 프로미스 체이닝으로 처리한다면 ?
+//-- 장점 1.
+//) then 메서드 연속 사용 => 순차적인 작업 가능
+//) 콜백지옥에서 탈출 !
+
+//-- 장점 2.
+//) 예외처리 간편
+
+function add(n1, n2) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      const result = n1 + n2; // 14
+      resolve(result); // resolve(14)
+    }, 1000);
+  });
+}
+
+function mul(n) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      const result = n * 2;
+      // resolve(result);
+      reject(new Error('의도적으로 발생시킨 에러!')); // 에러 메세지 발생
+    }, 700);
+  });
+}
+
+function sub(n) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      const result = n - 1;
+      resolve(result);
+    }, 500);
+  });
+}
+
+add(4, 3)
+  .then(function (result) {
+    console.log('1: ', result); // 7
+    return mul(result); // return mul(7)  //) return 꼭 써주기 !
+  })
+  .then(function (result) {
+    console.log('2: ', result); // 14
+    return sub(result); // return sub(14)
+  })
+  .then(function (result) {
+    console.log('3: ', result); // 13
+  })
+  .catch(function (err) {
+    console.log(err); // add - mul(reject new Error)
+  });
