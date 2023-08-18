aa426f9fc4b2ee232e8c21607a29c85fbe4f93ee
loveflora <love7620640@naver.com>
2023-08-18 14:43:10 +0900

    <study> ajax

--- 11-dynamic-form/app.js
+++ 11-dynamic-form/app.js
@@ -4,18 +4,36 @@ const app = express();
 const PORT = 8000;
 
 app.set('view engine', 'ejs');
-app.set('/views', 'views');
+
+//_ 정적파일 (image, css, js) --> static/
+// app.use('/public', express.static(__dirname + '/static'));
+//_ '/public' : 실행이 될 주소
+
+app.set('/views', express.static(__dirname + '/views'));
 
 //; 미들웨어 등록
 app.use(express.urlencoded({ extended: true })); //) post 요청으로 들어오는 모든 형식의 데이터를 파싱(분해)
 app.use(express.json()); //) json 형식으로 데이터를 주고 받음
-// json : object와 유사하지만, key에 큰 따옴표 존재
 
 //; 라우팅(Routing) - 주소 설정
 app.get('/', (req, res) => {
   console.log(req.query); // {}
   //) res.render(ejs파일경로, 데이터)
   res.render('dynamic');
+});
+
+// ajax로 서버에 get 요청하면
+// 서버가 응답 보냄
+app.get('/ajax', (req, res) => {
+  console.log(req.query);
+  // 브라우저에서 확인할 수 있는 코드가 아님 !
+  // 브라우저 바깥에서 실행하는 코드 (터미널에서 확인가능 - 서버)
+  res.send(req.query);
+});
+
+app.post('/ajax', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
 });
 
 app.listen(PORT, () => {
