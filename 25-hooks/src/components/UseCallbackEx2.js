3923e65877bd30d21aacdadbaea4b5533dd86847
saywhat <95282021+loveflora@users.noreply.github.com>
2023-10-06 19:47:53 +0900

    refactor : 추가 주석 설명보충

--- 25-hooks/src/components/UseCallbackEx2.js
+++ 25-hooks/src/components/UseCallbackEx2.js
@@ -39,12 +39,16 @@ export default function UseCallbackEx2({ postId }) {
   //) => useEffect가 계속 불러짐 ===> useCallback 훅 사용해서 함수를 저장
 
   // useEffect 의존성 배열에 "함수"
-  // 컴포넌트가 리렌더링 (...?) -> 함수 재생성 (주소값 변경) -> getPost 재호출
+  // 함수가 호출되어 API 요청 -> 데이터 받아오면서 컴포넌트가 리렌더링 -> 함수 재생성 (주소값 변경) -> getPost 재호출
 
   // xxxx const getPost 정의하면서, getPost 함수가 변경된다고 인식
   // xxxx useEffect에서 getPost() 실행하면서
 
   // useEffect의 의존성 배열에 포함된 값들이 변경될 때마다 useEffect의 콜백 함수가 실행됩니다.
+
+  // [ 초기 렌더링(initial render) 시 ]
+  // getPost 함수가 초기화되고, useEffect는 해당 함수를 실행하게 됩니다.
+  // 그러므로 초기화 화면에서도 getPost 함수가 호출되어 API 요청이 발생하고, 따라서 컴포넌트가 리렌더링되는 것입니다.
 
   // 주소값이 바뀌고, 렌더링되고, 바뀌고, 렌더링되고, ...
 
--- 25-hooks/src/hooks/useTitle.js
+++ 25-hooks/src/hooks/useTitle.js
@@ -1,20 +1,56 @@
 //=== Custom Hook 1 ===
-// Component 아닌 Hook임 !
 
 import React, { useEffect } from 'react';
 
 export default function useTitle(title) {
+  //_ 1. 라우터 사용하면 prevTitle 필요함
+  // react는 페이지 단위로 생성
   useEffect(() => {
+    // console.log(document.title); // 변경 전
+    // console.log(title); // 변경 후
+
+    // swap처럼 prevTitle이랑 title을 바꿈
     const prevTitle = document.title;
-    document.title = title;
-    // swap처럼 바꾼 것 같음..?
+
+    document.title = title; // 새로운 제목으로 변경
 
     // 원래대로 되돌려 놓음
-    return () => (document.title = prevTitle);
+    return () => (document.title = prevTitle); // 이전 제목으로 되돌리기
+    // 사라질 때 코드
   }, [title]);
 
-  // react는 페이지 단위로 생성
+  //* [ prevTitle 사용하는 이유 ]
+  // 라우터를 사용하는 경우 페이지 간에 제목을 변경해야 할 때 prevTitle과 함께 사용하는 것이 일반적인 방법입니다.
 
+  // 라우터를 사용하면 다양한 페이지가 동적으로 렌더링되고,
+  // 각 페이지마다 서로 다른 페이지 제목을 가질 수 있습니다.
+  // 따라서 컴포넌트가 unmount될 때 이전 페이지의 제목으로 돌아가기 위해서 prevTitle 변수를 사용하는 것이 유용합니다.
+
+  //* [ return "이전" vs "이후"의 document.title = title ]
+  //* useEffect에서의 return 함수는 ?
+  // useEffect 훅의 콜백 함수 내부에서 반환한 함수는,
+  // 해당 컴포넌트가 unmount(사라질 때)될 때 실행되는 clean-up 함수입니다.
+
+  // 따라서 return "이후"의 함수는 컴포넌트가 unmount 될 때 실행됩니다.
+  // 여기에서는 페이지 제목을 이전 제목인 title로 되돌리는 역할을 합니다.
+
+  // useTitle 훅을 사용하는 컴포넌트가 화면에 렌더링되고,
+  // 해당 페이지의 제목을 변경한 후에 다른 페이지로 이동하면 컴포넌트가 unmount 됩니다.
+  // 이때 return 다음에 오는 함수가 실행되어 페이지 제목을 이전 값인 title로 돌려놓게 됩니다.
+
+  // 이러한 clean-up 함수는 메모리 누수를 방지하고 애플리케이션의 상태를 올바르게 관리하는 데 도움을 줍니다.
+
+  //_ 2. prevTitle 사용 안해도 변경되기는 함
+  // useEffect(() => {
+  //   // const prevTitle = document.title;
+  //   document.title = title;
+  //   console.log(document.title); // 변경 전
+  //   console.log(title); // 변경 후
+  //   // return () => (document.title = prevTitle);
+  //   return () => (document.title = title);
+  // }, [title]);
+
+  //_ Component 아닌 Hook임 !
   //\ return <div>useTitle</div>;
   // hook이라서 return 하는 것은 없고, 함수를 return 하는 것임
 }
