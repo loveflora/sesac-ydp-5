bd4b92e8d1f49e83f168054de0d53376a6dbfb4f
saywhat <95282021+loveflora@users.noreply.github.com>
2023-10-18 14:27:11 +0900

    feat: redux 초기 설정

---
+++ 30-redux/src/App2.js
@@ -0,0 +1,73 @@
+//=== Redux ===
+import { useSelector, useDispatch } from 'react-redux';
+import './styles/Box.css';
+
+// import { useState } from 'react';
+
+function App() {
+  //_ [ BEFORE : Props Drilling ]
+  // const [number, setNumber] = useState(100);
+  // const plus = () => setNumber(number + 1);
+  // const minus = () => setNumber(number - 1);
+
+  const number = useSelector((state) => state.number);
+
+  return (
+    <div className="App">
+      <h1>React Redux</h1>
+      {/* <h2>Props Drilling</h2> */}
+      {/* <Box1 number={number} plus={plus} minus={minus} /> */}
+      <Box1 />
+
+      <h2>number: {number}</h2>
+    </div>
+  );
+}
+
+//] 컴포넌트 1. Box1
+// const Box1 = ({ number, plus, minus }) => {
+const Box1 = () => {
+  return (
+    <div className="Box">
+      <h2>Box1</h2>
+      {/* <Box2 number={number} plus={plus} minus={minus} /> */}
+      <Box2 />
+    </div>
+  );
+};
+
+//] 컴포넌트 2. Box2
+const Box2 = () => {
+  return (
+    <div className="Box">
+      <h2>Box2</h2>
+      <Box3 />
+    </div>
+  );
+};
+
+//] 컴포넌트 3. Box3
+const Box3 = () => {
+  return (
+    <div className="Box">
+      <h2>Box3</h2>
+      <Box4 />
+    </div>
+  );
+};
+
+//] 컴포넌트 4. Box4
+const Box4 = () => {
+  const number = useSelector((state) => state.number);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="Box">
+      <h2>Box4: {number}</h2>
+      <button onClick={() => dispatch({ type: 'PLUS' })}>PLUS</button>
+      <button onClick={() => dispatch({ type: 'MINUS' })}>MINUS</button>
+    </div>
+  );
+};
+
+export default App;
--- 30-redux/src/index.js
+++ 30-redux/src/index.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App2 from './App2';
+
+//=== Redux ===
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit'; // store를 정의하는 메소드
+import { composeWithDevTools } from 'redux-devtools-extension'; // redux 확장 프로그램
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// state 초기값 정의
+const initialState = {
+  number: 50,
+};
+
+//] reducer 정의 : 변화를 일으키는 함수
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'PLUS':
+      return { number: state.number + 1 };
+    case 'MINUS':
+      return { number: state.number - 1 };
+    default:
+      return state;
+  }
+};
+
+//] store 정의 : 전역 상태를 관리하는 공간 (하나의 프로젝트에 하나만 !)
+const store = configureStore({ reducer }, composeWithDevTools());
+// configureStore(redux 객체, redux 확장 프로그램)
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App2 />
+    </Provider>
   </React.StrictMode>
 );
